d8a64cb0fd23612213a75b4b0b2cd2728a52dd9a
khodary94 <khodary94@aucegypt.edu>
2015-05-09 09:13:48 +0300

    VerilogParser

---
+++ VParser/VParser/Node.cpp
@@ -0,0 +1,9 @@
+//
+//  Node.cpp
+//  VParser
+//
+//  Created by Mahmoud Khodary on 5/9/15.
+//  Copyright (c) 2015 Mahmoud Khodary. All rights reserved.
+//
+
+#include "Node.h"
---
+++ VParser/VParser/Node.h
@@ -0,0 +1,23 @@
+//
+//  Node.h
+//  VParser
+//
+//  Created by Mahmoud Khodary on 5/9/15.
+//  Copyright (c) 2015 Mahmoud Khodary. All rights reserved.
+//
+
+#ifndef __VParser__Node__
+#define __VParser__Node__
+
+#include <stdio.h>
+#include <iostream>
+#include <string>
+
+class Node
+{
+private:
+    std::string name;
+    float weightDelay;
+};
+
+#endif /* defined(__VParser__Node__) */
